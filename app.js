b6924d265a8210042da46086a4bced7b1b803f76
alexv7 <alexvarjabedian7@gmail.com>
2018-05-25 13:53:10 -0700

    using data and methods with vuejs

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 new Vue({
   el: '#vue-app',
   data: {
-    name: 'Alex'
+    name: 'Alex',
+    job: 'work'
+  },
+  methods: {
+    greet: function(time_of_day){
+      // this # refers to the vue instance, technically to use the data above in the methods you would need to do this.data.name, but vue-js has simplified that for us and you now need to only do this.name
+      return 'Good ' + time_of_day + ' ' + this.name
+    }
   }
 });
