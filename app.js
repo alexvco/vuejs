284e251000206ccb6109ff67142ca0c6d3aa139e
alexv7 <alexvarjabedian7@gmail.com>
2018-05-26 14:55:41 -0700

    bind data to different attributes, v-bind and v-html

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el: '#vue-app',
   data: {
     name: 'Alex',
-    job: 'work'
+    job: 'work',
+    website: 'http://www.google.com',
+    websiteTag: '<a href="http://www.google.com" target="_blank">Google website</a>'
   },
   methods: {
     greet: function(time_of_day){
@@ -11,3 +13,7 @@ new Vue({
     }
   }
 });
+
+// data binding: basically if you want to include website data above as the value of an a tag's href, you need to bind it
+// the way to bind the data is via a directive: an instruction such as v-bind:href that tells vuejs to basically bind the href attribute
+// to some dynamic data that we have stored on our view instance, there is also a shortcut you can do :href instead of v-bind:href
