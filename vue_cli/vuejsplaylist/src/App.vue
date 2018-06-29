9df26c7f8a5d1df0344c896d4db011ce39553254
Alex Varjabedian <alex@trueandco.com>
2018-06-29 10:47:32 -0700

    Nesting and importing components

--- vue_cli/vuejsplaylist/src/App.vue
+++ vue_cli/vuejsplaylist/src/App.vue
@@ -3,11 +3,18 @@
   <div>
     <h1>{{ title}}</h1>
     <p>{{ greeting() }}</p>
+    <my_ninjas_component></my_ninjas_component>
   </div>
 </template>
 
 <script>
+// import Ninjas from './Ninjas.vue' // This is to import/use components locally
+
 export default {
+  // This is to import/use components locally
+  // components: {
+  //   'my_ninjas_component': Ninjas
+  // },
   data: function () {
     return {
       title: 'First Vue file'
--- vue_cli/vuejsplaylist/src/main.js
+++ vue_cli/vuejsplaylist/src/main.js
@@ -1,6 +1,10 @@
 // import is from ES6 syntax, which is using webpack / babel to transpile es6 to vanilla js
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue' // This is your root component, you can nest other components insice this component
+
+// This is how you register the Ninjas component globally, basically can be used anywhere in your app
+import Ninjas from './Ninjas.vue'
+Vue.component('my_ninjas_component', Ninjas)
 
 // we are creating a new vue instance here which will render the component App.vue (see import above) into #app div in index.html file
 new Vue({
@@ -8,5 +12,5 @@ new Vue({
   render: h => h(App)
 })
 
-// basically a .vue file is just an extension of a component
+// basically a .vue file is just (an extension of) a component
 // component: re-usable template (functionality/code)
