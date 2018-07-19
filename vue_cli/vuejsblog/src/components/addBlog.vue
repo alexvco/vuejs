501bc8d95b355fb36992882031c6562d609c433c
Alex Varjabedian <alex@trueandco.com>
2018-07-18 23:21:52 -0700

    Select box Binding: using v-model on the select tag to store a property and display it in preview

--- vue_cli/vuejsblog/src/components/addBlog.vue
+++ vue_cli/vuejsblog/src/components/addBlog.vue
@@ -17,6 +17,10 @@
   			<label for="">Cheese</label>
   			<input type="checkbox" value="cheese" v-model="blog.categories">
    		</div>
+   		<label>Author</label>
+   		<select v-model="blog.author">
+   			<option v-for="author in authors">{{author}}</option>
+   		</select>
   	</form>
   	<div id="preview">
   		<h3>Preview Blog</h3>
@@ -27,6 +31,7 @@
   		<ul>
   			<li v-for="category in blog.categories">{{category}}</li>
   		</ul>
+  		<p>Author: {{blog.author}}</p>
   	</div>
 
   </div>
@@ -39,8 +44,10 @@
 	    	blog: {
 		    	title: "",
 		    	content: "",
-		    	categories: []
-		    }
+		    	categories: [],
+		    	author: ""
+		    },
+		    authors: ["Alex", "Jack", "Mike"]
 	    }
 	  },
 	  methods: {
