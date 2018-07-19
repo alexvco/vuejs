35471fbdb8079cb48c991db08133515bd15c3552
Alex Varjabedian <alex@trueandco.com>
2018-07-19 15:25:58 -0700

    Http requests: submitting blog via post request using vue-resource library

--- vue_cli/vuejsblog/src/components/addBlog.vue
+++ vue_cli/vuejsblog/src/components/addBlog.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="blog">
   	<h2>Add a New Blog Post</h2>
-  	<form action="">
+  	<form action="" v-if="!submitted">
   		<label>Blog Title:</label>
   		<!-- .lazy basically inserts it in preview on tab, rather than on keyup which is the default -->
   		<input type="text" v-model.lazy="blog.title" required>
@@ -21,7 +21,17 @@
    		<select v-model="blog.author">
    			<option v-for="author in authors">{{author}}</option>
    		</select>
+   		<br>
+   		<br>
+	  	<!-- We will want to submit this form and eventually send it to our db,  -->
+			<!-- we can use jquery ajax post to do it but in this example we will use vue-resource -->
+			<!-- (npm install vue-resource --save) to handle http requests (get, post)  -->
+			<button v-on:click.prevent="post_this_blog">Add Blog</button>
+			<!-- the .prevent is a modifier thats basically saying preventDefault -->
   	</form>
+  	<div v-if="submitted">
+  		<h3>Thanks for adding your post</h3>
+  	</div>
   	<div id="preview">
   		<h3>Preview Blog</h3>
   		<p>Blog Title: {{blog.title}}</p>
@@ -33,7 +43,6 @@
   		</ul>
   		<p>Author: {{blog.author}}</p>
   	</div>
-
   </div>
 </template>
 
@@ -47,11 +56,27 @@
 		    	categories: [],
 		    	author: ""
 		    },
-		    authors: ["Alex", "Jack", "Mike"]
+		    authors: ["Alex", "Jack", "Mike"],
+		    submitted: false
 	    }
 	  },
 	  methods: {
-
+	  	post_this_blog: function(){
+	  		// this refers to the component
+	  		// $http is from VueResource
+	  		// post is the http method
+	  		// the second param we pass to the post method is an object containing the data, in this case the params expected by jsonplaceholder api
+	  		// remember when we enter something into the form, the properties are populated because of the v-model binding
+	  		// $http.post returns to us a promise, therefore we use .then, the data/response that we get back in .then(function(response) is typically the data that we sent in post request
+	  		this.$http.post("https://jsonplaceholder.typicode.com/posts", {
+	  			title: this.blog.title,
+	  			body: this.blog.content,
+	  			userId: 1
+	  		}).then(function(response){
+	  			console.log(response.body.title);
+	  			this.submitted = true;
+	  		})
+	  	}
 	  }
 	}
 </script>
