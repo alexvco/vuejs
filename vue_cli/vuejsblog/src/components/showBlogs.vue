eed483fe70e2c7907082ef916ff98072fbc62ac2
alexv7 <alexvarjabedian7@gmail.com>
2018-07-20 13:09:09 -0700

    Custom Search Filter: technically we are using computed properties and not Vue.filter to search/filter through blogs

--- vue_cli/vuejsblog/src/components/showBlogs.vue
+++ vue_cli/vuejsblog/src/components/showBlogs.vue
@@ -3,7 +3,8 @@
   <!-- and will not be able to find it in the script below and hence will give error in the console -->
   <div v-theme:column="'wide'" id="show-blogs">
     <h1>All Blog Articles</h1>
-    <div v-for="blog in blogs" class="single-blog">
+    <input type="text" v-model="search" placeholder="search blogs">
+    <div v-for="blog in filteredBlogs" class="single-blog">
       <h2 v-rainbow>{{blog.title | to-uppercase }}</h2>
       <article>{{blog.body | snippet }}</article>
     </div>
@@ -14,7 +15,8 @@
   export default {
     data: function() {
       return {
-        blogs: []
+        blogs: [],
+        search: ""
       }
     },
     methods: {
@@ -26,6 +28,20 @@
         console.log(response.body); // an array of 100 articles/posts in json format
         this.blogs = response.body.slice(0, 10); // this will give us the first 10 articles from that array
       })
+    },
+    // We will use computed property to create a custom search filter
+    // Technically we are not using a Vue.filter, but a computed property to "search/filter" for blogs
+    // Reminder a computed property is similar to a method but caches the result into a value as opposed to calculating it everytime like a method
+    computed: {
+      filteredBlogs: function(){
+        // the following returns an array of blogs that match the search input, by filtering out blogs that do not match the search input
+        return this.blogs.filter(function (blog) {
+          // if the following returns true, then that blog stays in the array (meaning the search term is in the blog)
+          return blog.title.match(this.search)
+        }.bind(this))
+        // had to bind this as its inside second function and this no longer refers to the component.
+        // you can also use fat arrow .filter((blog) => {}) which automatically binds this for you
+      }
     }
   }
 </script>
