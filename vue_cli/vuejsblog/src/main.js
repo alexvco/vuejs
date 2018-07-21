0f1e097bc563a1e3f5a3a3794dba0fda65147d99
alexv7 <alexvarjabedian7@gmail.com>
2018-07-21 09:44:15 -0700

    Routing modes: hash (/#/mypath) vs history (/mypath)

--- vue_cli/vuejsblog/src/main.js
+++ vue_cli/vuejsblog/src/main.js
@@ -7,16 +7,29 @@ import Routes from './routes' // we need to import this as we chose to keep our 
 Vue.use(VueResource);
 Vue.use(VueRouter);
 
+
 // We need to create a new instance of the VueRouter that we Vue.use
 // We pass it an object, which will take our routes so basically we need to register our routes
 // We could register the routes here, but I prefer registering them outside in their own files (routes.js) to be more organized
+
+// Since our route uses the hash mode by default (/#/mypath), we need to inform it so that it uses the history mode (/mypath), so that our url's are cleaner
+// The reason vue uses the hash(#) in front of the request (or url rather), its because it is not making an additional request to the server
+// its actually very similar to clicking/targeting an id (href="#myid")on the page which will navigate to the section/part of the page that starts/has that id.
+// It is not making another request to the server (thats how hash (#) works). Hence thats why vuejs uses this hash (because its not making requests to the server)
+// In order to get rid of the hash, you need to setup your server so that no matter what the url or path is, it always returns the index.html file (single-page js application which is the point of using React, Vue and Angular)
+// All of the routing is controlled by the front end. Vuejs takes care of the path and routing (whatever is placed after the '/', vuejs can deal with that by rendering the appropriate components)
+// A good guide on how to setup your server to always return index.html file no matter what the url/path is (https://router.vuejs.org/guide/essentials/history-mode.html)
+// Fortunately for us our development server that we are using with vuejs automatically returns the index.html page to us every time.
+// So all we need to do (in our dev env), is add a mode property with a value of history. (In prod, you also need to sort out your server using the guide link above)
+// Extra - A good read: https://en.wikipedia.org/wiki/Single-page_application
 const router = new VueRouter({
-	routes: Routes 
+	routes: Routes, 
 	// we could have registered the routes here as follows
 	// routes: [
 	//   {path: "/", component: showBlogs}, 
 	//   {path: "/blogs/new", component: addBlog}
 	// ]
+	mode: 'history' // by default this is set to hash
 });
 
 
